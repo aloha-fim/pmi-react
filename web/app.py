436185c96400102260ac8b3819d21e1d96b1a388
aloha-fim <frederec.im@gmail.com>
2023-03-31 17:23:36 -1000

    prototype setup

--- web/app.py
+++ web/app.py
@@ -6,23 +6,25 @@ import bcrypt
 app = Flask(__name__)
 api = Api(app)
 
-#db
 client = MongoClient("mongodb://db:27017")
 db = client.SimilarityDB
 users = db["Users"]
 
 def UserExist(username):
-    if users.count_documents({"Username":username}) == 0;
+    # if users.find({"Username":username}).count() == 0:
+    if users.count_documents({"Username":username}) == 0:
         return False
     else:
         return True
-    
+
 class Register(Resource):
     def post(self):
+        #Step 1 is to get posted data by the user
         postedData = request.get_json()
 
+        #Get the data
         username = postedData["username"]
-        password = postedData["password"]
+        password = postedData["password"] #"123xyz"
 
         if UserExist(username):
             retJson = {
@@ -30,7 +32,7 @@ class Register(Resource):
                 'msg': 'Invalid Username'
             }
             return jsonify(retJson)
-        
+
         hashed_pw = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
 
         #Store username and pw into the database
@@ -45,11 +47,11 @@ class Register(Resource):
             "msg": "You successfully signed up for the API"
         }
         return jsonify(retJson)
-    
+
 def verifyPw(username, password):
     if not UserExist(username):
         return False
-        
+
     hashed_pw = users.find({
         "Username":username
     })[0]["Password"]
@@ -58,17 +60,15 @@ def verifyPw(username, password):
         return True
     else:
         return False
-        
+
 def countTokens(username):
     tokens = users.find({
         "Username":username
     })[0]["Tokens"]
     return tokens
-    
-    
+
 class Detect(Resource):
     def post(self):
-
         #Step 1 get the posted data
         postedData = request.get_json()
 
@@ -84,8 +84,7 @@ class Detect(Resource):
                 'msg': "Invalid Username"
             }
             return jsonify(retJson)
-            
-        #step 3 verify the username pw mach
+        #Step 3 verify the username pw match
         correct_pw = verifyPw(username, password)
 
         if not correct_pw:
@@ -94,27 +93,27 @@ class Detect(Resource):
                 "msg": "Incorrect Password"
             }
             return jsonify(retJson)
-        
-        #Step 4 verify user has enough tokens
+        #Step 4 Verify user has enough tokens
         num_tokens = countTokens(username)
         if num_tokens <= 0:
             retJson = {
                 "status": 303,
                 "msg": "You are out of tokens, please refill!"
-                }
+            }
             return jsonify(retJson)
-        
+
         #Calculate edit distance between text1, text2
         import spacy
         nlp = spacy.load('en_core_web_sm')
         text1 = nlp(text1)
-        test2 = nlp(text2)
+        text2 = nlp(text2)
 
-        # Ration is a number between 0 and 1 the closer to 1, the more similar text1 and text 2 are
+        # Ratio is a number between 0 and 1 the closer to 1, the more similar text 1 
+        # and text 2 are
         ratio = text1.similarity(text2)
 
         retJson = {
-            "status": 200,
+            "status":200,
             "ratio": ratio,
             "msg":"Similarity score calculated successfully"
         }
@@ -122,7 +121,7 @@ class Detect(Resource):
         #Take away 1 token from user
         current_tokens = countTokens(username)
         users.update_one({
-            "Username": username
+            "Username":username
         }, {
             "$set":{
                 "Tokens":current_tokens-1
@@ -130,7 +129,7 @@ class Detect(Resource):
         })
 
         return jsonify(retJson)
-    
+
 class Refill(Resource):
     def post(self):
         postedData = request.get_json()
@@ -145,33 +144,35 @@ class Refill(Resource):
                 "msg": "Invalid Username"
             }
             return jsonify(retJson)
-        
-        correct_pw = 'abc123'
+
+        correct_pw = "abc123"
         if not password == correct_pw:
             retJson = {
                 "status":304,
                 "msg": "Invalid Admin Password"
             }
             return jsonify(retJson)
-        
+
         #MAKE THE USER PAY!
         users.update_one({
             "Username":username
         }, {
             "$set":{
-                "Tokens": refill_amount
+                "Tokens":refill_amount
                 }
         })
 
         retJson = {
             "status":200,
-            "msg": "Refillled successfully"
+            "msg": "Refilled successfully"
         }
         return jsonify(retJson)
-    
+
+
 api.add_resource(Register, '/register')
 api.add_resource(Detect, '/detect')
 api.add_resource(Refill, '/refill')
+
 
 if __name__=="__main__":
     app.run(host='0.0.0.0')
